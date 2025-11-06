4ece11dd8971aec70b975c344a5e3b4b1a916a3c
cnguyen1244-ctrl <cnguyen1244@student.cccd.edu>
2025-11-06 13:05:28 -0800

    string stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#incude <string>
 using namespce std;
 int main()
 {
     cout << "Hello world." << endl;
     cout << "This is fun!" << endl;
+
+    string s = "abc";
+    cout << s << endl;
 }
